d34ad89b7774a331f76b173f13d9dd95889d955a
PolHernáez <alu.pol.hernaez@mataro.epiaedu.cat>
2025-11-13 17:53:10 +0100

    Variables ajuntades

--- PE05.java
+++ PE05.java
@@ -3,16 +3,10 @@ import java.util.Scanner;
 public class PE05 {
     Scanner entrada = new Scanner(System.in);
     boolean menuA = true;
-    String linea = "";
-    String nomProducte = "";
-    String nomClient = "";
-    int numeroTicket = 1;
-    double preuUnitari = 0;
-    int quantitat = 0;
-    String resposta = "";
-    double total = 0;
+    String linea = "", nomProducte = "", nomClient = "", resposta = "";
+    int numeroTicket = 1, espaiTicket = 0, quantitat = 0;
+    double preuUnitari = 0, total = 0;
     static String comandaGuardada = "";
-    int espaiTicket = 0;
     public static void main(String[] args) {
         PE05 p = new PE05();
         p.principal();
@@ -152,14 +146,12 @@ public class PE05 {
     public void guardarComanda() {
         double subtotal = (preuUnitari * quantitat);
         total = total + subtotal;
-        linea = linea + "\n" + numeroTicket + "\t" + nomProducte + igualarTicket(nomProducte, espaiTicket = 20) + preuUnitari + "€" + igualarTicket(preuUnitari + "€", espaiTicket = 20) + quantitat + igualarTicket(quantitat + "€", espaiTicket = 20) + subtotal + "€" + "\n";
+        linea = linea + "\n" + numeroTicket + "\t" + nomProducte + igualarTicket(nomProducte, 20) + preuUnitari + "€" + igualarTicket(preuUnitari + "€",  20) + quantitat + igualarTicket(quantitat + "€",  20) + subtotal + "€" + "\n";
         numeroTicket++;
-    }
-    
+    }    
     public void comandaGuardadaM(String nomClient, String linea, double total) { // Guardar comanda completa
-        comandaGuardada = comandaGuardada + "\n" + "TICKET" + "\n" + "Client: " + nomClient + "\n" + "============================================================================" + "\n" + "NÚM\tPRODUCTE            PREU UNITARI        QUANTITAT          SUBTOTAL" + "\n" + linea + "============================================================================" + "\n" + "TOTAL SENSE IVA: " + igualarTicket(total + "€", espaiTicket = 59) + total + "€" + "\n" + "IVA (10%): " + igualarTicket((total * 0.1) + "€", espaiTicket = 65) + (total * 0.1) + "€" + "\n" + "TOTAL: " + igualarTicket((total + (total * 0.1)) + "€", espaiTicket = 69) + (total + (total * 0.1)) + "€" + "\n" + "============================================================================";
+        comandaGuardada = comandaGuardada + "\n" + "TICKET" + "\n" + "Client: " + nomClient + "\n" + "============================================================================" + "\n" + "NÚM\tPRODUCTE            PREU UNITARI        QUANTITAT          SUBTOTAL" + "\n" + linea + "============================================================================" + "\n" + "TOTAL SENSE IVA: " + igualarTicket(total + "€", 59) + total + "€" + "\n" + "IVA (10%): " + igualarTicket((total * 0.1) + "€", 65) + (total * 0.1) + "€" + "\n" + "TOTAL: " + igualarTicket((total + (total * 0.1)) + "€",  69) + (total + (total * 0.1)) + "€" + "\n" + "============================================================================";
     }
-    
     public String igualarTicket(String paraula, int espai) { // Afegir espais per a igualar columnes
         String blanc = "";
         for (int i = paraula.length(); i < espai; i++) {
